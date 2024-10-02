e698e4ff17bdc43f7fbd764f88769bb7d9ace242
Shivam Bisht <rajshivam2745@gmail.com>
2024-10-02 23:47:27 +0530

    useDebounce without useEffect

--- useDebounce/src/components/useDebounce.js
+++ useDebounce/src/components/useDebounce.js
@@ -1,17 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState, useCallback } from "react";
 
 function useDebounce(value, delay) {
   const [debouncedValue, setDebouncedValue] = useState(value || "");
 
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [value]);
+  // when we are calling the debounce function every time it is rendering it is creating different function
+  // so when we are typing like hello the result is showing letter by letter instead of a particular word
+  // this is happening because we wanted the debounceFun which is taking the args to run only one time
+  const debouncedFun = useCallback(debounce(setDebouncedValue, delay), []);
+  debouncedFun(value);
+  // useEffect(() => {
+  //   const timerId = setTimeout(() => {
+  //     setDebouncedValue(value);
+  //   }, delay);
+  //   return () => {
+  //     clearTimeout(timerId);
+  //   };
+  // }, [value]);
   return debouncedValue;
 }
 
 export default useDebounce;
+
+function debounce(callback, delay) {
+  let timerId;
+  return function debouncedFun(...args) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
